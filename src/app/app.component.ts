37ed5a8fdac81adc2e2dda73e8176b67df59cdc4
Ramya <ramya.bala221190@gmail.com>
2024-02-29 18:01:27 +0530

    updated dependencies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, ElementRef, ViewChild,HostListener } from '@angular/core';
-import { EventTrackDirective, getProvider } from 'click-tracker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
-  providers:[getProvider(AppComponent)]
+  styleUrls: ['./app.component.scss']
 
 })
 export class AppComponent {
