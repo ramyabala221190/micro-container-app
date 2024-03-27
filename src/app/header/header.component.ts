4ad1145a48f99a2b02d5eba8734d46691f58e5cf
Ramya <ramya.bala221190@gmail.com>
2024-03-27 15:22:42 +0530

    added environment details

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ConfigService, appConfigModel } from '../config.service';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+
+  constructor(private configService:ConfigService){
+
+  }
+
+  environment:string|undefined="";
+
+  ngOnInit(){
+    let configurationDetails:appConfigModel|undefined=this.configService.retreiveConfigurationDetails();
+    this.environment=configurationDetails?.env;
+  }
+
 
 }
